2b0bd0fd1df78c10718a106c08949e2d09155285
saichiro17 <pilotonjoseph862@gmail.com>
2024-10-27 12:59:59 +0800

    added decription

--- applet5.js
+++ applet5.js
@@ -6,6 +6,7 @@ class WeatherApp{
         this.weatherCard = document.getElementById('weatherCard');
         this.cityName = document.getElementById('cityName');
         this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
 
     }
 }
