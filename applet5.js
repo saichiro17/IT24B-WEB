3b8c2e94f98b3813e45aa7121c6d31fea0527349
saichiro17 <pilotonjoseph862@gmail.com>
2024-10-27 13:01:21 +0800

    added fetchweather

--- applet5.js
+++ applet5.js
@@ -11,5 +11,7 @@ class WeatherApp{
         this.windSpeed = document.getElementById('windSpeed');
         this.APIKeyInput = document.getElementById('apiInput');
 
+        this.getWeatherBtn.addEventListener('click', () => this.fetchWeather());
+
     }
 }
