43f4f4b1295bcb4be830b857195f8b22621e0027
saichiro17 <pilotonjoseph862@gmail.com>
2024-10-27 12:58:11 +0800

    added cityinput and getweatherbtn

--- applet5.js
+++ applet5.js
@@ -1,4 +1,7 @@
 class WeatherApp{
     constructor(){
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
+
     }
 }
