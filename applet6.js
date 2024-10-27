cb72c77e082e24e0f24891249268884f7fdce9d2
saichiro17 <pilotonjoseph862@gmail.com>
2024-10-27 14:12:30 +0800

    added contructor input

--- applet6.js
+++ applet6.js
@@ -2,6 +2,7 @@ class TodoList {
     constructor() {
         this.editingIndex = -1;
         this.addButton = document.getElementById('addButton');
+        this.todoInput = document.getElementById('todoInput');
 
     }
 }
